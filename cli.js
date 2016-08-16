819866c883d62c079135915ae0d2c430e7e986a0
Jared Palmer <jaredpalmer>
2016-08-16 18:31:49 -0400

    init

---
+++ cli.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+var electron = require('electron')
+var spawn = require('cross-spawn')
+var path = require('path')
+var chalk = require('chalk')
+
+process.stdout.write('\x1bc')
+console.log()
+console.log()
+console.log(chalk.cyan('Compiling...'))
+
+var child = spawn(electron, [path.join(__dirname, '../main.js')] , { stdio: 'inherit' })
+
+child.on('close', function (code) {
+  if (code !== 0) {
+     console.error('Could not bundle prototype.')
+     console.log()
+     console.log()
+     return
+  }
+})
---
+++ readme.md
@@ -0,0 +1,15 @@
+# framer-electron-preview (a.k.a 'fep')
+
+**Quickly run Framer prototypes within Electron.**
+
+## How to use
+```bash
+# to install globally on your computer...
+npm i -g framer-electron-preview
+
+# Open your Framer.js prototype in terminal...
+cd MyPrototype.framer
+fep
+```
+
+#### Questions? Hit me up on Twitter: <a href="htts://twitter.com/jaredpalmer">jaredpalmer</a>
